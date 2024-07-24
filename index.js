34d738302ba37fd7a22c2b86f3f442daf36a42c8
Giovanni8793 <neri66543@gmail.com>
2024-07-24 09:41:57 -0600

    yaml created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { Admin } from "./src/components/Admin";
-import { MyElement } from "./src/components/MyElement";
-import { ModalComponent } from "./src/components/Modal"; 
+import { Admin } from "/src/components/Admin";
+import { MyElement } from "/src/components/MyElement";
+import { ModalComponent } from "/src/components/Modal"; 
 
 
 
